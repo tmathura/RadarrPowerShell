f5f7408f38a4bf871b8fc84213c68159ff0ceaa8
Teshvier Mathura <teshvier.mathura@datasnipper.com>
2023-03-31 15:33:27 +0200

    add log file to repo

--- radarr_unmonitor.ps1
+++ radarr_unmonitor.ps1
@@ -2,26 +2,27 @@ $expected_eventtype = 'Download'
 $release_groups = @('lama','rarbg','psa')
 $quality = '265'
 $doApiCall = $false;
+$log_file = ('{0}\raddarr_powershell.log') -f ($PSScriptRoot)
 
 #$env:radarr_eventtype = 'Download'
 #$env:radarr_moviefile_relativepath = 'Cloverfield.2008.1080p.BluRay.x265-LAMA.mp4'
 #$env:radarr_movie_id = 1821
 
-('{0} - In event ''' + $env:radarr_eventtype + ''' for movie file ''' + $env:radarr_moviefile_relativepath + '''') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+('{0} - In event ''' + $env:radarr_eventtype + ''' for movie file ''' + $env:radarr_moviefile_relativepath + '''') -f (Get-Date) | Out-File -FilePath $log_file -Append
 
 if ($env:radarr_eventtype -eq $expected_eventtype) {
-  ('{0} - In expected event ''' + $expected_eventtype + '''') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+  ('{0} - In expected event ''' + $expected_eventtype + '''') -f (Get-Date) | Out-File -FilePath $log_file -Append
 
   for ( $index = 0; $index -lt $release_groups.count; $index++)
   {
       if ($env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($release_groups[$index])) -And $env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($quality))) {
-        ('{0} - Release group ''' + $release_groups[$index] + ''' found') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+        ('{0} - Release group ''' + $release_groups[$index] + ''' found') -f (Get-Date) | Out-File -FilePath $log_file -Append
         $doApiCall = $true;
       }
   }
 
   if ($doApiCall) {
-    ('{0} - Initiate API call') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+    ('{0} - Initiate API call') -f (Get-Date) | Out-File -FilePath $log_file -Append
 
     $headers = @{}
     $headers.Add("accept", "*/*")
@@ -36,9 +37,9 @@ if ($env:radarr_eventtype -eq $expected_eventtype) {
     
     $response = Invoke-WebRequest -Uri 'http://localhost:7878/api/v3/movie/editor' -Method PUT -Headers $headers -ContentType 'application/json' -Body $body
 
-    ('{0} - Completed API call') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+    ('{0} - Completed API call') -f (Get-Date) | Out-File -FilePath $log_file -Append
   }
   else {
-    ('{0} - API not called') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+    ('{0} - API not called') -f (Get-Date) | Out-File -FilePath $log_file -Append
   }
 }
