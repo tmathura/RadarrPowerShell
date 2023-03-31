3642c6ab010f8b77a5ca1c42bd358e5c3e755a89
Teshvier Mathura <teshvier.mathura@datasnipper.com>
2023-03-31 15:20:37 +0200

    use array for release groups

--- radarr_unmonitor.ps1
+++ radarr_unmonitor.ps1
@@ -1,8 +1,6 @@
 $expected_eventtype = 'Download'
-$search_text_1 = 'lama'
-$search_text_2 = 'rarbg'
-$search_text_3 = 'psa'
-$search__quality = '265'
+$release_groups = @('lama','rarbg','psa')
+$quality = '265'
 $doApiCall = $false;
 
 #$env:radarr_eventtype = 'Download'
@@ -12,21 +10,14 @@ $doApiCall = $false;
 ('{0} - In event ''' + $env:radarr_eventtype + ''' for movie file ''' + $env:radarr_moviefile_relativepath + '''') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
 
 if ($env:radarr_eventtype -eq $expected_eventtype) {
-  ('{0} - In expected if statement') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+  ('{0} - In expected event ''' + $expected_eventtype + '''') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
 
-  if ($env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search_text_1)) -And $env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search__quality))) {
-    ('{0} - Search text 1 ''' + $search_text_1 + ''' found') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
-    $doApiCall = $true;
-  }
-
-  if ($env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search_text_2)) -And $env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search__quality))) {
-    ('{0} - Search text 2 ''' + $search_text_2 + ''' found') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
-    $doApiCall = $true;
-  }
-
-  if ($env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search_text_3)) -And $env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($search__quality))) {
-    ('{0} - Search text 1 ''' + $search_text_3 + ''' found') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
-    $doApiCall = $true;
+  for ( $index = 0; $index -lt $release_groups.count; $index++)
+  {
+      if ($env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($release_groups[$index])) -And $env:radarr_moviefile_relativepath -like ('*{0}*' -f [WildcardPattern]::Escape($quality))) {
+        ('{0} - Release group ''' + $release_groups[$index] + ''' found') -f (Get-Date) | Out-File -FilePath C:\radarr_unmonitor.txt -Append
+        $doApiCall = $true;
+      }
   }
 
   if ($doApiCall) {
